5689d89e6d01a517362284b8a64c3ac4370ec3a2
Maciej Lelusz <maciej.lelusz@inleo.pl>
2020-02-25 15:04:24 +0100

    Create variables.tf

---
+++ org-vdc-payg/variables.tf
@@ -0,0 +1,68 @@
+variable "vcd_user" {
+  type = string
+  description = "Username of admin account."
+  default = "administrator"
+}
+
+variable "vcd_pass" {
+  type = string
+  description = "Password of admin user."
+}
+
+variable "vcd_org" {
+  type = string
+  description = "For provider choose default."
+  default = "System"
+}
+
+variable "vcd_url" {
+  type = string
+  description = "Address of your vCD API."
+  default = "https://vcd.lab.inleo.pl/api"
+}
+
+variable "vcd_max_retry_timeout" {
+  type = string
+  default = "60"
+}
+
+variable "vcd_allow_unverified_ssl" {
+  type = bool
+  default = "true"
+}
+
+variable "vcd_org_name" {
+  type = string
+  description = "Name of your org."
+}
+
+variable "vcd_org_vdc_name" {
+  type = string
+  description = "Name of your vDC."
+}
+
+variable "vcd_org_vdc_description" {
+  type = string
+  description = "Description of your vDC."
+}
+
+variable "vcd_org_vdc_network_pool_name" {
+  type = string
+  description = "Name of Network Pool."
+  default = ""
+}
+
+variable "vcd_org_vdc_provider_vdc_name" {
+  type = string
+  description = "Name of Provider VDC."
+}
+
+variable "vcd_org_vdc_storage_profile_name" {
+  type = string
+  description = "Name of your Storage Profile."
+}
+
+variable "vcd_org_vdc_storage_profile_limit" {
+  type = string 
+  description = "Limit of Mb of your Storage Profile."
+}
